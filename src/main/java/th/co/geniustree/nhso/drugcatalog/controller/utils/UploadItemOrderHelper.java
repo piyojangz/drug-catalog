9a9bc485acaaa663bf8f19895a985854d0e49ca0
pramoth <pramoth@geniustree.co.th>
2014-07-25 05:16:09 +0700

    -order E and U item by changedate and updatedate.

--- src/main/java/th/co/geniustree/nhso/drugcatalog/controller/utils/UploadItemOrderHelper.java
+++ src/main/java/th/co/geniustree/nhso/drugcatalog/controller/utils/UploadItemOrderHelper.java
@@ -6,6 +6,9 @@
 package th.co.geniustree.nhso.drugcatalog.controller.utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.ListIterator;
 import th.co.geniustree.nhso.drugcatalog.model.UploadHospitalDrugItem;
@@ -25,11 +28,31 @@ public class UploadItemOrderHelper {
             if ("A".equalsIgnoreCase(next.getUpdateFlag())) {
                 flagA.add(next);
                 listIterator.remove();
-            }else if("D".equalsIgnoreCase(next.getUpdateFlag())){
+            } else if ("D".equalsIgnoreCase(next.getUpdateFlag())) {
                 flagDelete.add(next);
                 listIterator.remove();
             }
         }
+        //TODO makeit testable.
+        Collections.sort(items, new Comparator<UploadHospitalDrugItem>() {
+
+            @Override
+            public int compare(UploadHospitalDrugItem o1, UploadHospitalDrugItem o2) {
+                Date date1;
+                Date date2;
+                if ("U".equalsIgnoreCase(o1.getUpdateFlag())) {
+                    date1 = o1.getDateUpdateDate();
+                } else {
+                    date1 = o1.getDateChangeDate();
+                }
+                if ("U".equalsIgnoreCase(o1.getUpdateFlag())) {
+                    date2 = o2.getDateUpdateDate();
+                } else {
+                    date2 = o2.getDateChangeDate();
+                }
+                return date1.compareTo(date2);
+            }
+        });
         items.addAll(0, flagA);
         items.addAll(flagDelete);
     }
