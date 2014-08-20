a416be654f82638dc070f052071a67ff582b9473
pramoth <pramoth@geniustree.co.th>
2014-08-20 17:10:08 +0700

    -add druggroup

---
+++ site/druggroup.sql
@@ -0,0 +1,21 @@
+--------------------------------------------------------
+--  File created - Tuesday-August-19-2014   
+--------------------------------------------------------
+REM INSERTING into TMT_DRUGGROUP
+SET DEFINE OFF;
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('OCPA',to_timestamp('19-AUG-14 06.36.01.884000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยามะเร็ง',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('RDPA',to_timestamp('19-AUG-14 06.36.14.842000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยา Rheumatic Disease Prior Authorization',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('DDPA',to_timestamp('19-AUG-14 06.36.32.627000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยา Dermatology Disease Prior Authorization',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('KT_REJECTION',to_timestamp('19-AUG-14 06.36.47.305000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยา Kidney Transplant with rejection',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('J2',to_timestamp('19-AUG-14 06.37.03.949000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยา ATG, IVIG',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('MI',to_timestamp('19-AUG-14 06.37.15.023000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยา stroke STEMI',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('AA',to_timestamp('19-AUG-14 06.37.25.801000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยา Severe Aplastic Anemia',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('PI',to_timestamp('19-AUG-14 06.37.38.498000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยาลดการเป็นแผลและเลือดออกในกระเพาะอาหาร Anti-ulcerant /variceal bleeding',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('NSAIDS',to_timestamp('19-AUG-14 06.37.51.047000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยาต้านอักเสบที่มิใช่สเตียรอยด์ NSAIDs/ anti-osteoarthritis',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('ANTIL',to_timestamp('19-AUG-14 06.38.09.791000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยาลดไขมันในเลือด Antilipidemia',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('ACEI',to_timestamp('19-AUG-14 06.38.28.028000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยาเบื้องต้นในการรักษาความดันโลหิตสูงและภาวะหัวใจล้มเหลวแบบเลือดคั่ง Angiotensin converting enzyme (ACE) inhibitors',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('ARBS',to_timestamp('19-AUG-14 06.38.47.294000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยาลดความดันโลหิต Angiotensin-II receptor blockers (ARBs)',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('ANTIP',to_timestamp('19-AUG-14 06.39.02.061000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยาป้องกันการเกาะตัวของเกล็ดเลือด Antiplatelets',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('GLUC',to_timestamp('19-AUG-14 06.39.17.932000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'ยาลดข้อเข่าเสื่อม Glucosamine',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('BONE',to_timestamp('19-AUG-14 06.39.31.788000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'ยาป้องกันโรคกระดูกพรุน Drug affecting bone metabolism',1);
+Insert into TMT_DRUGGROUP (ID,CREATEDATE,DESCRIPTION,VERSION) values ('CANCER',to_timestamp('19-AUG-14 06.39.47.043000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'กลุ่มยารักษามะเร็ง Anti-cancers',1);
