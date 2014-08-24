0fbcc3902b679a512bb3b1fa6b1a6b558fa77650
pramoth <pramoth@geniustree.co.th>
2014-08-24 13:50:09 +0700

    -Use spring paging in index page.

--- src/main/java/th/co/geniustree/nhso/drugcatalog/controller/admin/AdminInbox.java
+++ src/main/java/th/co/geniustree/nhso/drugcatalog/controller/admin/AdminInbox.java
@@ -10,7 +10,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+import th.co.geniustree.nhso.drugcatalog.controller.SpringDataLazyDataModelSupport;
 import th.co.geniustree.nhso.drugcatalog.model.RequestItem;
 import th.co.geniustree.nhso.drugcatalog.repo.RequestItemRepo;
 
@@ -24,19 +27,20 @@ public class AdminInbox implements Serializable {
 
     @Autowired
     private RequestItemRepo requestItemRepo;
-    private List<RequestItem> requestItems;
+    private SpringDataLazyDataModelSupport<RequestItem> requestItems;
 
     @PostConstruct
     public void postConstruct() {
-        requestItems = requestItemRepo.findByStatus(RequestItem.Status.REQUEST);
-    }
+        requestItems = new SpringDataLazyDataModelSupport<RequestItem>(){
 
-    public List<RequestItem> getRequestItems() {
+            @Override
+            public Page<RequestItem> load(Pageable pageAble) {
+                return requestItemRepo.findByStatus(RequestItem.Status.REQUEST,pageAble);
+            }
+            
+        }; 
+    }
+    public SpringDataLazyDataModelSupport<RequestItem> getRequestItems() {
         return requestItems;
     }
-
-    public void setRequestItems(List<RequestItem> requestItems) {
-        this.requestItems = requestItems;
-    }
-    
 }
--- src/main/java/th/co/geniustree/nhso/drugcatalog/repo/RequestItemRepo.java
+++ src/main/java/th/co/geniustree/nhso/drugcatalog/repo/RequestItemRepo.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package th.co.geniustree.nhso.drugcatalog.repo;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import th.co.geniustree.nhso.drugcatalog.model.RequestItem;
 
@@ -14,8 +14,8 @@ import th.co.geniustree.nhso.drugcatalog.model.RequestItem;
  *
  * @author moth
  */
-public interface RequestItemRepo extends JpaRepository<RequestItem, Integer>{
+public interface RequestItemRepo extends JpaRepository<RequestItem, Integer> {
 
-    public List<RequestItem> findByStatus(RequestItem.Status status);
-    
+    public Page<RequestItem> findByStatus(RequestItem.Status status, Pageable pageable);
+
 }
