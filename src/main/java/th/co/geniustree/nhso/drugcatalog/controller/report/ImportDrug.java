ebf28053ffa522d34402d8d7dc460b57a22f7225
pramoth <pramoth@geniustree.co.th>
2014-09-05 10:58:34 +0700

    -remove unused import.

--- src/main/java/th/co/geniustree/nhso/drugcatalog/controller/report/ImportDrug.java
+++ src/main/java/th/co/geniustree/nhso/drugcatalog/controller/report/ImportDrug.java
@@ -5,18 +5,10 @@
  */
 package th.co.geniustree.nhso.drugcatalog.controller.report;
 
-import java.io.Serializable;
-import java.util.List;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
-import th.co.geniustree.nhso.basicmodel.readonly.Hospital;
-import th.co.geniustree.nhso.basicmodel.readonly.Province;
-import th.co.geniustree.nhso.basicmodel.readonly.Zone;
 import th.co.geniustree.nhso.drugcatalog.repo.UploadHospitalDrugRepo;
-import th.co.geniustree.nhso.drugcatalog.service.NhsoZoneService;
 
 /**
  *
