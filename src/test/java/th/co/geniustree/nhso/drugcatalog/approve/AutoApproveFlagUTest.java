4f43686aa5b509a70b9e9569ec87d70c89688404
Thanthathon Buagird <thanthathon@geniustree.co.th>
2016-05-11 16:01:38 +0700

    test unit price mustn't more than twice as much, use mockito

---
+++ src/test/java/th/co/geniustree/nhso/drugcatalog/approve/AutoApproveFlagUTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.nhso.drugcatalog.approve;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+import th.co.geniustree.nhso.drugcatalog.model.UploadHospitalDrug;
+import th.co.geniustree.nhso.drugcatalog.model.UploadHospitalDrugItem;
+import th.co.geniustree.nhso.drugcatalog.repo.UploadHospitalDrugItemRepo;
+import th.co.geniustree.nhso.drugcatalog.service.UploadHospitalDrugItemService;
+import th.co.geniustree.nhso.drugcatalog.service.impl.UploadHospitalDrugItemServiceImpl;
+
+/**
+ *
+ * @author thanthathon.b
+ */
+public class AutoApproveFlagUTest {
+
+    private UploadHospitalDrugItemService uploadHospitalDrugItemService;
+    private UploadHospitalDrugItem newItem;
+
+    @Before
+    public void setUp() {
+        newItem = new UploadHospitalDrugItem();
+        newItem.setUploadDrug(Mockito.mock(UploadHospitalDrug.class));
+        UploadHospitalDrugItem latestItem = new UploadHospitalDrugItem();
+        latestItem.setUnitPrice("10");
+        latestItem.setUpdateFlag("U");
+
+        uploadHospitalDrugItemService = new UploadHospitalDrugItemServiceImpl();
+        UploadHospitalDrugItemRepo uploadHospitalDrugItemRepo = Mockito.mock(UploadHospitalDrugItemRepo.class);
+
+        Mockito.when(uploadHospitalDrugItemRepo.findLatestItemThatAcceptAndNotDeleteByUpdateFlag(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(latestItem);
+        ReflectionTestUtils.setField(uploadHospitalDrugItemService, "repo", uploadHospitalDrugItemRepo);
+
+    }
+
+    @Test
+    public void oldPrice10_newPrice15_MustApprove() {
+        newItem.setUnitPrice("15");
+        boolean notMoreThanTwiceAsMuch = uploadHospitalDrugItemService.isUnitPriceNotMoreThanDoubleLatestItem(newItem);
+        Assertions.assertThat(notMoreThanTwiceAsMuch)
+                .isTrue();
+    }
+
+    @Test
+    public void oldPrice10_newPrice20_MustApprove() {
+        newItem.setUnitPrice("20");
+        boolean notMoreThanTwiceAsMuch = uploadHospitalDrugItemService.isUnitPriceNotMoreThanDoubleLatestItem(newItem);
+        Assertions.assertThat(notMoreThanTwiceAsMuch)
+                .isTrue();
+    }
+
+    @Test
+    public void oldPrice10_newPrice21_MustNotApprove() {
+        newItem.setUnitPrice("21");
+        boolean notMoreThanTwiceAsMuch = uploadHospitalDrugItemService.isUnitPriceNotMoreThanDoubleLatestItem(newItem);
+        Assertions.assertThat(notMoreThanTwiceAsMuch)
+                .isFalse();
+    }
+    
+}
