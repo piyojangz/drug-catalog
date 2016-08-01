7f3532b46a88e62f5cb5fff562200402b7df9564
Thanthathon Buagird <thanthathon@geniustree.co.th>
2016-08-01 16:23:42 +0700

    add new Role SUPER_ADMIN for delete some master data

--- src/main/java/th/co/geniustree/nhso/drugcatalog/authen/Role.java
+++ src/main/java/th/co/geniustree/nhso/drugcatalog/authen/Role.java
@@ -20,4 +20,5 @@ public interface Role {
     public static final GrantedAuthority HOSPITAL = new SimpleGrantedAuthority("HOSPITAL");
     public static final GrantedAuthority ECLAIM = new SimpleGrantedAuthority("ECLAIM");
     public static final GrantedAuthority EMCO = new SimpleGrantedAuthority("EMCO");
+    public static final GrantedAuthority SUPER_ADMIN = new SimpleGrantedAuthority("SUPER_ADMIN");
 }
